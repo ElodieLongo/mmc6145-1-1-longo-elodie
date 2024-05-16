ce9aa3add80267008680b27360557ba56c51a6bf
ElodieLongo <133866010+ElodieLongo@users.noreply.github.com>
2024-05-16 09:08:47 +0200

    css

---
+++ src/pages/contact.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/contact.module.css'
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className={styles.titles}>Let's get in touch</h1>
+    </main>
+  )
+}
