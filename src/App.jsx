c273aebf96b307ed220ea5535272ecc03d30e51b
ElodieLongo <mail.elodielongo@gmail.com>
2024-05-17 17:27:59 +0200

    image imported on home

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact/>} />
       </Routes>
     </>
   )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,15 @@
 import styles from '../styles/home.module.css'
+import Project1 from '../assets/Project1.jpg'
+
 
 export default function Home() {
   return (
+  
     <main>
       <h1 className={styles.title}>I'm the home page!</h1>
+
+      <img src={Project1} alt="Project1" width="300" height="auto"/>
     </main>
+    
   )
 }
